b052aca146b1759ce4aad3c2719847379928726f
LeeZ1Q <466944567@qq.com>
2023-07-16 22:38:49 +0800

    feat:copy btn for code md

--- app/components/markdown.tsx
+++ app/components/markdown.tsx
@@ -4,6 +4,27 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import rehypeHighlight from 'rehype-highlight';
 import remarkGfm from 'remark-gfm';
+import { useRef } from 'react';
+import { copyToClipboard } from '../utils';
+
+export function PreCode(props: { children: any }) {
+	const ref = useRef<HTMLPreElement>(null);
+
+	return (
+		<pre ref={ref}>
+			<span
+				className=''
+				onClick={() => {
+					if (ref.current) {
+						const code = ref.current.innerText;
+						copyToClipboard(code);
+					}
+				}}
+			></span>
+			{props.children}
+		</pre>
+	);
+}
 
 export function Markdown(props: { content: string }) {
 	return (
